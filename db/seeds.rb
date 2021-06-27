7a3d7ded052554bd8702234c08dbf14217228365
OM Miles <miles@ommiles.com>
2021-06-27 14:24:58 -0700

    seeds written

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,33 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+50.times do
+    User.create(
+        name: Faker::Name.name_with_middle,
+        email: Faker::Internet.free_email,
+        password_digest: "1",
+        img_url: Faber::Avatar.image,
+        favorite_genre: Faker::Book.genre,
+        age: rand(18...110),
+        bio: Faker::Dune.quote,
+        latitude: Faker::Address.latitude,
+        longitude: Faker::Address.longitude
+    )
+end
+
+200.times do
+    Book.create(
+        title: Faker::Book.title,
+        author: Faker::Book.author,
+        description: Faker::Company.catch_phrase,
+        genre: Faker::Book.genre,
+        isbn: Faker::Code.isbn,
+        publisher: Faker::Book.publisher,
+        user: User.ids.sample
+    )
+end
+
+200.times do
+    Review.create(
+        content: Faker::Hipster.sentence,
+        book: Book.ids.sample,
+        user: User.ids.sample
+    )
+end
