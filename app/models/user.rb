31bb2a208a40a6ff02e68cb5cb6b1e9eef956c79
OM Miles <miles@ommiles.com>
2021-06-27 18:14:30 -0700

    add validations on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
     has_many :books
     has_many :reviews
+    has_secure_password
+    validates :name, presence: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :email, uniqueness: { case_sensitive: false }
 end
