6524b4ae23bd9f2feba3b3e306fa029d723e9caa
OM Miles <miles@ommiles.com>
2021-06-27 22:07:32 -0700

    add controller actions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::API
+
+    before_action :authorized
+
+    def encode_token
+    end
+
+    def auth_header
+    end
+
+    def decoded_token
+    end
+
+    def current_user
+    end
+
+    def logged_in?
+    end
+
+    def authorized
+    end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,38 @@
 class UsersController < ApplicationController
 
+    skip_before_action :authorized, only: [:create, :login, :getUser]
+
     def index
         users = User.all
         render json: users, except: [:password_digest]
+    end
+
+    # def new
+    # end
+
+    def create
+    end
+
+    def show
+    end
+
+    # def edit
+    # end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def login
+    end
+
+    def getUser
+    end
+
+    private
+
+    def user_params
     end
 end
